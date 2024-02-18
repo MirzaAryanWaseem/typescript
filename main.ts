3176f8da1ebe48891bf916e7fad1c2dc6ff2b919
MirzaAryanWaseem <aryanbaig293@gmail.com>
2024-02-18 12:25:46 +0500

    firstprogram

---
+++ main.ts
@@ -0,0 +1,18 @@
+console.log("Hello World");
+
+// let console=10
+
+
+var englishnum=100
+var englishnum=200
+
+
+var a
+
+let a=10;
+
+ a=190;
+
+
+
+const 
---
+++ script.js
@@ -0,0 +1,22 @@
+// var a=10
+
+// var a; //declare
+
+// a=10 //assigna
+// a=20
+// console.log(a);
+
+// var a=10;
+
+// let a;
+
+// a=10
+
+// let a;
+
+
+
+// const a=10;
+
+// const a
+
